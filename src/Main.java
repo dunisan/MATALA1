534da024ccc0c617ac16bf57d7f761a2c655939e
dunisan <Danielnisan155@gmail.com>
2022-11-07 23:08:47 +0200

    M

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String error = null;
+        UndoableStringBuilder usb = new UndoableStringBuilder();
+        usb.append("to be or not to be") ;
+        System.out.println(usb) ;
+        usb.replace(3, 5, "eat") ;
+        System.out.println(usb) ;
+        usb.delete(3,6) ;
+        usb.replace(17, 19, "eat") ;
+        System.out.println(usb) ;
+        usb.reverse();
+        System.out.println(usb) ;
+        System.out.println();
+        usb.undo();
+        System.out.println(usb);
+        usb.undo();
+        System.out.println(usb) ;
+        usb.undo();
+        System.out.println(usb);
+
+    }
+}
---
+++ src/UndoableStringBuilderTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UndoableStringBuilderTest {
+    @Test
+    void insertNegative(){
+
+
+    }
+}
